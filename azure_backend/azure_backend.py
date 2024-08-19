e52d1989d328a248d915df90f0544c1dcd36f80b
PSala <polsalalidon@gmail.com>
2024-08-19 11:58:04 +0200

    Add get_object method on backend

--- azure_backend/azure_backend.py
+++ azure_backend/azure_backend.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import logging
 
 logger = logging.getLogger('openerp.' + __name__)
@@ -11,3 +12,10 @@ def get_azure_blob_client():
         raise ValueError('Missing Azure Blob Storage configuration')
     client = BlobServiceClient.from_connection_string(connection_string)
     return client
+
+
+def get_object(client, container_name, object_path):
+    container_client = client.get_container_client(container_name)
+    blob_client = container_client.get_blob_client(object_path)
+    downloader = blob_client.download_blob()
+    return downloader.readall()
